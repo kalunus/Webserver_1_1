cfe0bc673c92aba15ea2ef863469389806dcedbc
SBT-Boyko-AP <kindervud@gmail.com>
2017-09-14 12:23:16 +0300

    Добавлен сервлет mirror

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -4,15 +4,18 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import servlets.AllRequestsServlet;
+import servlets.MirrorRequestsServlet;
 
 /**
  * Created by SBT-Boyko-AP on 11.09.2017.
  */
 public class Main {
     public static void main(String[] args) throws Exception {
+        MirrorRequestsServlet mirrorRequestsServlet = new MirrorRequestsServlet();
         AllRequestsServlet allRequestsServlet = new AllRequestsServlet();
 
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(mirrorRequestsServlet), "/mirror");
         context.addServlet(new ServletHolder(allRequestsServlet), "/*");
 
         Server server = new Server(8080);
---
+++ src/test/java/PageGeneratorTest.java
@@ -0,0 +1,5 @@
+/**
+ * Created by SBT-Boyko-AP on 14.09.2017.
+ */
+public class PageGeneratorTest {
+}
