933f2f3fe75ffdcc8d319e35bb24f80bd5df25b6
SBT-Boyko-AP <kindervud@gmail.com>
2017-09-14 16:01:19 +0300

    Добавлен сервлет mirror

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,10 +1,16 @@
 package main;
 
+import com.sun.media.jfxmedia.logging.Logger;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.RequestLog;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.log.LoggerLog;
+import org.eclipse.jetty.util.log.StdErrLog;
 import servlets.AllRequestsServlet;
 import servlets.MirrorRequestsServlet;
+import sun.rmi.runtime.Log;
 
 /**
  * Created by SBT-Boyko-AP on 11.09.2017.
@@ -22,7 +28,12 @@ public class Main {
         server.setHandler(context);
 
         server.start();
+        //System.out.println("Server started");
+        context.getLogger().info("Server started");
         server.join();
-        System.out.println("Server started");
+
+
+        //Logger.logMsg(Logger.INFO,"Server started");
+
     }
 }
